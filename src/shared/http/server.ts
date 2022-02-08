a157eb19b2a59953aebffb2313a0872b65236545
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-02-07 21:50:36 -0300

    feat: add custom errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import { isError } from './middlewares/isError';
 import { routes } from './routes/index.routes';
 
 export const app = express();
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use(isError);
 app.listen(3333, () => {
   console.log('✅ Server started in http://localhost:3333');
 });
