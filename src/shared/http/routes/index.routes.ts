eb78b22ae81d768defbf0716480d234832d38773
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-02-11 22:20:50 -0300

    feat: add sessions route

--- src/shared/http/routes/index.routes.ts
+++ src/shared/http/routes/index.routes.ts
@@ -1,6 +1,8 @@
+import { sessionsRouter } from '@modules/users/routes/sessions.routes';
 import { userRouter } from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
 export const routes = Router();
 
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionsRouter);
