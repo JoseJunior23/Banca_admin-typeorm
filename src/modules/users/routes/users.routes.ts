db05d572cb38b9a0b7380dace053f4ea981dedf0
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-02-14 22:58:09 -0300

    feat: add route file uploads

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,16 @@
+import uploadConfig from '@config/upload';
 import { isAuthenticated } from '@shared/http/middlewares/isAuthenticated';
 import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express';
+import multer from 'multer';
+import { UserAvatarController } from '../controllers/UserAvatarController';
 import { UsersController } from '../controllers/UsersController';
 
 export const userRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 userRouter.get('/', isAuthenticated, usersController.index);
 
@@ -18,4 +24,11 @@ userRouter.post(
     },
   }),
   usersController.create,
+);
+
+userRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
 );
