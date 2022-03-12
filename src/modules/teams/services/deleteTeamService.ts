1c740acaf36e43ec9418ad43c0f9031a83bdef01
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-03-11 22:21:24 -0300

    feat: add team delete service

---
+++ src/modules/teams/services/deleteTeamService.ts
@@ -0,0 +1,21 @@
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { TeamsRepository } from '../repositories/TeamsRepository';
+
+interface ITeam {
+  id: string;
+}
+
+export class DeleteTeamService {
+  public async execute({ id }: ITeam) {
+    const teamRepository = getCustomRepository(TeamsRepository);
+
+    const team = await teamRepository.findById(id);
+    if (!team) {
+      throw new AppError('Team not found !!!');
+    }
+
+    await teamRepository.remove(team);
+    return team;
+  }
+}
