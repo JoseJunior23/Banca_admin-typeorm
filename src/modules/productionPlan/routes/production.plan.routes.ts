e11dfbb1c0084fc8d8c04258be46a26735650364
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-03-16 22:46:42 -0300

    feat: add router productionPlan

---
+++ src/modules/productionPlan/routes/production.plan.routes.ts
@@ -0,0 +1,57 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import { ProductionPlanController } from '../controllers/ProductionPlanController';
+
+export const productionPlanRouter = Router();
+const productionPlanController = new ProductionPlanController();
+
+productionPlanRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      variation: Joi.string().required(),
+      description: Joi.string().required(),
+      entry_date: Joi.date().required(),
+      factory_plan: Joi.number().required(),
+    },
+  }),
+  productionPlanController.create,
+);
+
+productionPlanRouter.get('/', productionPlanController.index);
+
+productionPlanRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  productionPlanController.show,
+);
+
+productionPlanRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      variation: Joi.string().required(),
+      description: Joi.string().required(),
+      entry_date: Joi.date().required(),
+      factory_plan: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  productionPlanController.update,
+);
+
+productionPlanRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  productionPlanController.delete,
+);
