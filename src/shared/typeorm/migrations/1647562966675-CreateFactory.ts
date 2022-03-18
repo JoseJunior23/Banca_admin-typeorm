f21f652149078037075a61e7d788958177d452bb
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-03-17 21:47:44 -0300

    feat: add migration factory

--- src/shared/typeorm/migrations/1647562966675-CreateFactory.ts
+++ src/shared/typeorm/migrations/1647562966675-CreateFactory.ts
@@ -1,11 +1,43 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateFactory1647562966675 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'factory',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'corporate_name',
+            type: 'varchar',
+          },
+          {
+            name: 'fantasy_name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('factory');
+  }
 }
