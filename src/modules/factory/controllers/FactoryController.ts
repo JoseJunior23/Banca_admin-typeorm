31b8f6720dc386e0feb956ae08bf2c7b2538fa4d
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-03-21 22:21:45 -0300

    feat: add factory controller

---
+++ src/modules/factory/controllers/FactoryController.ts
@@ -0,0 +1,54 @@
+import { Request, response, Response } from 'express';
+import { CreateFactoryService } from '../services/CreateFactoryService';
+import { DeleteFactoryService } from '../services/DeleteFactoryService';
+import { ListFactoryService } from '../services/ListFactoryService';
+import { ShowFactoryService } from '../services/ShowFactoryService';
+import { UpdateFactoryService } from '../services/UpdateFactoryService';
+
+export class FactoryController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { corporate_name, fantasy_name } = request.body;
+
+    const createFactory = new CreateFactoryService();
+    const factory = await createFactory.execute({
+      corporate_name,
+      fantasy_name,
+    });
+    return response.json(factory);
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listFactory = new ListFactoryService();
+    const factory = await listFactory.execute();
+    return response.json(factory);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showFactory = new ShowFactoryService();
+    const factory = await showFactory.execute({
+      id,
+    });
+    return response.json(factory);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { corporate_name, fantasy_name } = request.body;
+
+    const updateFactory = new UpdateFactoryService();
+    const factory = await updateFactory.execute({
+      id,
+      corporate_name,
+      fantasy_name,
+    });
+    return response.json(factory);
+  }
+
+  public async delete(request: Request) {
+    const { id } = request.params;
+    const deleteFactory = new DeleteFactoryService();
+    await deleteFactory.execute({ id });
+    return response.status(200).json({ message: 'Successfully deleted factory 👍' });
+  }
+}
