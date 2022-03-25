229f09c75280e01a568f8b789962727a473181c2
JoseJunior23 <juniorferreira1919@hotmail.com>
2022-03-24 21:32:46 -0300

    feat: add shoesModel migration

---
+++ src/shared/typeorm/migrations/1648166135123-CreateShoesModel.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateShoesModel1648166135123 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'shoes_model',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'reference',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'price_pair_shoes',
+            type: 'float',
+          },
+          {
+            name: 'price_pespontador',
+            type: 'float',
+          },
+          {
+            name: 'price_coladeira',
+            type: 'float',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('shoes_model');
+  }
+}
